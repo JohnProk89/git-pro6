183b1861c6f919b29f485c0cbc197cf995e739d7
Giannis Prokopiou <johnprok89@gmail.com>
2024-05-21 19:38:32 +0300

    Coding Factory

--- src/gr/aueb/cf/Hello.java
+++ src/gr/aueb/cf/Hello.java
@@ -3,7 +3,7 @@ package gr.aueb.cf;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, this is working and does not care if highlighted as error in the IDE!");
+        System.out.println("Hello Coding Factory");
         System.out.println("Duh");
     }
 }
