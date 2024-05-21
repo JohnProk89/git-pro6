432246546bc4b14a4e7a734004d21e4e0552d0e6
Giannis Prokopiou <johnprok89@gmail.com>
2024-05-21 19:23:49 +0300

    Duh

--- src/gr/aueb/cf/Hello.java
+++ src/gr/aueb/cf/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is working and does not care if highlighted as error in the IDE!");
+        System.out.println("Duh");
     }
 }
