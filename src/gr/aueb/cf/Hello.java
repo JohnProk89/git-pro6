ab6cd0866b263c55747432c3a77a92c3a7b462f8
Giannis Prokopiou <johnprok89@gmail.com>
2024-05-21 18:59:24 +0300

    banged

---
+++ src/gr/aueb/cf/Hello.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is working and does not care if highlighted as error in the IDE!");
+    }
+}
