789e18bcbad53f00327d460070c71ff37ab9cb55
Giannis Prokopiou <johnprok89@gmail.com>
2024-05-21 19:42:57 +0300

    Hello CF

--- src/gr/aueb/cf/Hello.java
+++ src/gr/aueb/cf/Hello.java
@@ -3,7 +3,7 @@ package gr.aueb.cf;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, this is working and does not care if highlighted as error in the IDE!");
+        System.out.println("Hello, CF");
         System.out.println("Duh");
     }
 }
